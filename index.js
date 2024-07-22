d614355a6d5c5fc058c72c7253c070a835f489fa
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-22 15:25:03 +0300

    index.js

--- index.js
+++ index.js
@@ -14,20 +14,26 @@ let selectingDrivers=[]
 selectingDrivers[0]=returnFirstTwoDrivers
 selectingDrivers[1]=returnLastTwoDrivers
 
+
+//Fn that takes one argument, a. It creates a new function that takes one argument, fare multiplied by n
 function createFareMultiplier(a){
     return function(fare){
         return a*fare
     }
 }
-
+//A fn that takes a fare as an argument. It uses the function to create a new function that doubles the fare.
 const fareDoubler = function(fare){
     return createFareMultiplier(2)(fare)
 } 
 
+//A fn that takes a fare as an argument. It uses the function to create a new function that multiplies the fare
+//by 3. Then, it invokes the new function with the fare 
 const fareTripler = function(fare){
     return createFareMultiplier(3)(fare)
 }
 
+//Fn that takes two arguments, b and c. It checks if c is equal to the function returnFirstTwoDrivers
+//and then returns the result of calling the respective function with the argument b.
 function selectDifferentDrivers(b,c){
     if(c===returnFirstTwoDrivers){
         return returnFirstTwoDrivers(b)
